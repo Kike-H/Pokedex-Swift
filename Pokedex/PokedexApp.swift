a5cae2471a077fc1ecce8738be564ddcecaf7c0d
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-09-09 11:50:00 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kike Hernandez D. on 09/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
